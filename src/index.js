22f1ddbfc2b6931a547f465c08e5ff7c312f8bfd
virat <viratt737@gmail.com>
2025-12-19 18:23:58 +0530

    write server code wher my server run

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+dotenv.config({
+    path:"./.env"
+})
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
