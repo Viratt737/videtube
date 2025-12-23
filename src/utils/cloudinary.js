866fb027da24a0573bb30471755e652975b98d84
virat <viratt737@gmail.com>
2025-12-23 11:23:11 +0530

    "write code for upload file in cloudinary"

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,23 @@
 const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 cloudinary.config({
     cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        });
+        // file hass been uploaded
+        console.log("file is uploaded in cloudinary !!",response.url);
+        return response;
+    } catch (error) {
+        fs.unlink(localFilePath); // remove loaclly saved file temporary file as the upload failed
+    } 
+}
+
+module.exports = {uploadOnCloudinary};
